af14f0494253b4b32e0e35949c6d29a1cd307dd7
Neil <1522674529@qq.com>
2022-12-17 20:58:21 +0800

    fix: 修复逻辑错误

--- src/Util.js
+++ src/Util.js
@@ -26,7 +26,7 @@ export function _html(str) {
  */
 export function _isEmpty(value) {
     return (
-        !value
+        !value ||
         (typeof value === "string" && value.trim().length === 0) ||
         (typeof value === "object" && Object.keys(value).length === 0)
     );
