fb2d7ed37ad09cfde0d1a6542512126e2b092d6e
Neil <1522674529@qq.com>
2022-12-17 16:43:32 +0800

    refactor: 抽取code展示组件

---
+++ src/components/Code.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { MessageType } from '../models/MessageInterface';
+const props = defineProps<{
+  highlight: boolean,
+  codemirrorOptions: any,
+  message: MessageType
+}>()
+</script>
+
+<template>
+  <div class="t-code">
+    <div v-if="highlight">
+      <highlightjs ref="highlightjs" autodetect :code="message.content" />
+    </div>
+    <div v-else-if="codemirrorOptions">
+      <codemirror :value="message.content" :options="codemirrorOptions" />
+    </div>
+    <div v-else style="background: rgb(39 50 58);">
+      <pre style="padding: 1em;margin: 0">
+      <code style="font-size: 15px" v-html="message.content"></code></pre>
+    </div>
+  </div>
+</template>
