3f93a8c8f19126bdd0a57a6f88c4e1221c718c07
Neil <1522674529@qq.com>
2022-12-20 21:04:00 +0800

    暂时去除json-viewer可编辑深度功能

--- src/components/JsonMessage.vue
+++ src/components/JsonMessage.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
 import { JsonViewer } from "vue3-json-viewer"
 import "vue3-json-viewer/dist/index.css"
-import { DataConstant } from '@/constants/TerminalConstants'
-import { reactive, ref, watchEffect } from 'vue'
 import { MessageType } from '../models/MessageInterface'
 
 
-const props = defineProps<{
+defineProps<{
   message: MessageType
 }>()
 
 const defaultDepth = 1
-const depth = ref(props.message.depth ?? defaultDepth)
-// watchEffect(() => {
-//   depth.value = props.message.depth ?? defaultDepth
-// })
-
-const jsonViewDepth = reactive(DataConstant.JsonViewDepth)
-
 const parseToJson = (obj: object | string) => {
   if (typeof obj === "object" && obj) {
     return obj
@@ -33,13 +24,7 @@ const parseToJson = (obj: object | string) => {
 
 <template>
   <span style="position: relative">
-    <span>cur depth: {{ depth }}</span>
-    <json-viewer :expand-depth="depth ?? ''" sort boxed copyable expanded :value="parseToJson(message.content)">
+    <json-viewer :expand-depth="message.depth ?? defaultDepth" sort boxed copyable expanded :value="parseToJson(message.content)">
     </json-viewer>
-    <select class="t-json-deep-selector" v-model="depth">
-      <!-- <option disabled selected label="Choose a display deep" value=""></option> -->
-      <option v-for="i in jsonViewDepth" :key="i" :label="`Deep ${i}`" :value="i">
-      </option>
-    </select>
   </span>
 </template>
