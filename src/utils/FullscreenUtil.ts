bb343e354be1c161db0596a72973c4e24dcf70b1
Neil <1522674529@qq.com>
2022-12-22 21:46:00 +0800

    vscode: 去除重复配置

--- src/utils/FullscreenUtil.ts
+++ src/utils/FullscreenUtil.ts
@@ -1,6 +1,6 @@
 import { _isSafari } from "@/Util"
 import { useFullscreen } from "@vueuse/core"
-import { nextTick, Ref, watchEffect } from "vue"
+import { Ref, watchEffect } from "vue"
 
 type SafariStyleCache = {
   position: string
@@ -32,38 +32,36 @@ export function useFullscreenLifecycle(fullscreen: Ref<boolean>, fullArea: Ref<H
   const container = fullArea.value
 
   watchEffect(() => {
-    nextTick(() => {
-      if (fullscreen.value) {
-        if (!container) {
-          return
-        }
-        //  进入全屏
-        if (_isSafari()) {
-          safariStyleCache = {
-            position: container.style.position,
-            width: container.style.width,
-            height: container.style.height,
-            left: container.style.left,
-            top: container.style.top,
-          }
-          container.style.position = defaultFullScreenStyle.position
-          container.style.width = defaultFullScreenStyle.width
-          container.style.height = defaultFullScreenStyle.height
-          container.style.left = defaultFullScreenStyle.left
-          container.style.top = defaultFullScreenStyle.top
-        }
-      } else {
-        if (!container) {
-          return
-        }
-        if (_isSafari()) {
-          container.style.position = safariStyleCache.position
-          container.style.width = safariStyleCache.width
-          container.style.height = safariStyleCache.height
-          container.style.left = safariStyleCache.left
-          container.style.top = safariStyleCache.top
-        }
+    if (fullscreen.value) {
+      if (!container) {
+        return
       }
-    })
+      //  进入全屏
+      if (_isSafari()) {
+        safariStyleCache = {
+          position: container.style.position,
+          width: container.style.width,
+          height: container.style.height,
+          left: container.style.left,
+          top: container.style.top,
+        }
+        container.style.position = defaultFullScreenStyle.position
+        container.style.width = defaultFullScreenStyle.width
+        container.style.height = defaultFullScreenStyle.height
+        container.style.left = defaultFullScreenStyle.left
+        container.style.top = defaultFullScreenStyle.top
+      }
+    } else {
+      if (!container) {
+        return
+      }
+      if (_isSafari()) {
+        container.style.position = safariStyleCache.position
+        container.style.width = safariStyleCache.width
+        container.style.height = safariStyleCache.height
+        container.style.left = safariStyleCache.left
+        container.style.top = safariStyleCache.top
+      }
+    }
   })
 }
