0e6b0ea7d64eef292ce5a4a940e34e55fa42ad34
tzfun <beifengtz@qq.com>
2023-03-18 21:45:32 +0800

    feat inactive时文本不可选

--- src/Terminal.vue
+++ src/Terminal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="t-container"
+  <div :class="'t-container ' + (_isActive() ? '' : 't-disable-select')"
        :style="_draggable() ? _getDragStyle() : 'width:100%;height:100%;border-radius:0;'"
        ref="terminalContainer">
     <div class="terminal">
